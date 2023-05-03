c03692e0e20033b0fd584b5d25a782227bbf6cb8
LU XIN <greyson.lu@topverse.world>
2023-05-04 03:37:41 +0800

    Update

--- config/pages/home/use-section-two.ts
+++ config/pages/home/use-section-two.ts
@@ -3,25 +3,14 @@ export function useSectionTwo() {
     title: 'home.sectionTwo.title',
     content: [
       {
-<<<<<<< Updated upstream
-        img: 'https://landvault.io/assets/logos/paypal.svg',
-        href: '#',
-        title: 'home.sectionTwo.content.title[0]',
+        img: 'https://muadao.build/assets/logo.7c101bb2.svg',
+        href: 'https://muadao.build/',
+        title: 'MUA DAO',
       },
       {
         img: 'https://landvault.io/assets/logos/paypal.svg',
         href: '#',
         title: 'home.sectionTwo.content.title[1]',
-=======
-        img: 'https://muadao.build/assets/logo.7c101bb2.svg',
-        href: 'https://muadao.build/',
-        title: 'MUA',
-      },
-      {
-        img: 'https://ir.china-tower.com/en/images/global/logo.svg',
-        href: 'https://www.china-tower.com/index',
-        title: 'ChinaTower',
->>>>>>> Stashed changes
       },
       {
         img: 'https://landvault.io/assets/logos/paypal.svg',
