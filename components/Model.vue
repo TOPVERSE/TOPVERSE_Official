638f16445a8713a993a108b0f9c426ffcb6bde0b
胡鑫 <1219654535@qq.com>
2023-05-26 14:53:49 +0800

    修改目录结构

--- components/Model.vue
+++ components/Model.vue
@@ -7,7 +7,7 @@ const anim = useAnimation({ from: 180, to: 0, duration: 5000 })
 <template>
   <div class="relative h-96 w-96 bg-transparent">
     <World color="transparent" default-light="studio">
-      <LingoModel src="/apple_watch/scene.gltf" :y="0" :rotation-z="anim" :box-visible="false" />
+      <LingoModel src="/model/apple_watch/scene.gltf" :y="0" :rotation-z="anim" :box-visible="false" />
       <OrbitCamera active :z="100" :y="80" :zoom="1" auto-rotate enable-damping />
     </World>
   </div>
